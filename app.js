7bf03de0276de9a6d307993d9d104dd7466adf75
chowdhuary1882 <chowdhuary1882>
2021-09-01 02:23:17 -0400

    se

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
-alert("hello");
+const student = {
+    stId : 01,
+    stName : "Arif",
+    stMajor : "Math",
+    isAReularStudent : true
+};
+
+const studentKey = Object.keys;
+const studentValue = Object.values;
+const studentEntries = Object.entries;
+
+console.log(student.stMajor);
